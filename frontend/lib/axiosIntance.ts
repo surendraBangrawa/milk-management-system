2caf3fdd7e63099b40c4f205962ac7090fa79c68
Dev <2019umt1842@mnit.ac.in>
2025-05-10 17:10:14 +0530

    changed_url

--- frontend/lib/axiosIntance.ts
+++ frontend/lib/axiosIntance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.2:8000",
+  baseURL: "https://milk-management-system.onrender.com",
   timeout: 10000,
 });
 
