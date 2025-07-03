8796e6701e3cfe927e8d09b17c4ffbb9dc7d4983
surendraBangrawa <surendrabangrawa2001@gmail.com>
2025-07-04 00:22:20 +0530

    deleted_page_tsx

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useTheme } from "@/context/ThemeContext";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function LandingPage() {
   const { theme, colors } = useTheme();
